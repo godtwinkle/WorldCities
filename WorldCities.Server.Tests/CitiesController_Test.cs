636172024f308b63cd7f6bdf8f2d076ce5c6f45b
陈玉林 <tnl04122001@gmail.com>
2024-10-14 00:43:10 +0700

    14/10/2024 00.43

--- WorldCities.Server.Tests/CitiesController_Test.cs
+++ WorldCities.Server.Tests/CitiesController_Test.cs
@@ -1,7 +1,36 @@
-﻿namespace WorldCities.Server.Tests
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCities.Server.Controllers;
+using WorldCities.Server.Data;
+using WorldCities.Server.Data.Models;
+
+namespace WorldCities.Server.Tests
 {
-    [Fact]
     public class CitiesController_Test
     {
+        [Fact]
+        public async Task GetCity()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>().UseInMemoryDatabase(databaseName: "WorldCities").Options;
+            using var context = new ApplicationDbContext(options);
+            context.Add(new City()
+            {
+                Id = 1,
+                CountryId = 1,
+                Lat = 1,
+                Lon = 1,
+                Name = "TestCity1"
+            });
+            context.SaveChanges();
+
+            var controller = new CitiesController(context);
+            City? city_existing = null;
+            City? city_notExisting = null;
+
+            city_existing = (await controller.GetCity(1)).Value;
+            city_notExisting = (await controller.GetCity(2)).Value;
+
+            Assert.NotNull(city_existing);
+            Assert.Null(city_notExisting);
+        }
     }
 }
